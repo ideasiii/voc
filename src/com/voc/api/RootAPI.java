c9eeee54ec24475135ed80fd22460575d78df228
ideasiii <jianjhihjhong@iii.org.tw>
2018-10-18 10:01:01 +0800

    push

--- src/com/voc/api/RootAPI.java
+++ src/com/voc/api/RootAPI.java
@@ -10,18 +10,18 @@ import org.json.JSONObject;
 public abstract class RootAPI {
 	private static final String TABLE_PRODUCT_REPUTATION = "ibuzz_voc.product_reputation";
 	private static final String TABLE_BRAND_REPUTATION = "ibuzz_voc.brand_reputation";
-	private static final Map<String, String> paramColumnMap = new HashMap<String, String>();
+	private static final Map<String, String> PARAM_COLUMN_MAP = new HashMap<String, String>();
 	static {
-		paramColumnMap.put("industry", "industry");
-		paramColumnMap.put("brand", "brand");
-		paramColumnMap.put("series", "series");
-		paramColumnMap.put("product", "product_name");
-		paramColumnMap.put("source", "source");
-		paramColumnMap.put("website", "website_id");
-		paramColumnMap.put("channel", "channel_id");
-		paramColumnMap.put("features", "features");
-		paramColumnMap.put("start_date", "date");
-		paramColumnMap.put("end_date", "date");
+		PARAM_COLUMN_MAP.put("industry", "industry");
+		PARAM_COLUMN_MAP.put("brand", "brand");
+		PARAM_COLUMN_MAP.put("series", "series");
+		PARAM_COLUMN_MAP.put("product", "product_name");
+		PARAM_COLUMN_MAP.put("source", "source");
+		PARAM_COLUMN_MAP.put("website", "website_id");
+		PARAM_COLUMN_MAP.put("channel", "channel_id");
+		PARAM_COLUMN_MAP.put("features", "features");
+		PARAM_COLUMN_MAP.put("start_date", "date");
+		PARAM_COLUMN_MAP.put("end_date", "date");
 	}
 	protected static final String PARAM_VALUES_SEPARATOR = ";";
 
@@ -36,7 +36,7 @@ public abstract class RootAPI {
 	}
 	
 	protected String getColumnName(String paramName) {
-		return paramColumnMap.get(paramName);
+		return PARAM_COLUMN_MAP.get(paramName);
 	}
 	
 
