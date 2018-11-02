5251d65b9736356840ea3ad2476f616ddb224da4
ideasiii <jianjhihjhong@iii.org.tw>
2018-11-02 11:39:41 +0800

    Add log in ApiFilter.java

--- src/com/voc/filter/ApiFilter.java
+++ src/com/voc/filter/ApiFilter.java
@@ -13,6 +13,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.voc.api.RootAPI;
 import com.voc.common.ApiResponse;
@@ -20,13 +22,14 @@ import com.voc.service.HttpService;
 import com.voc.service.impl.HttpServiceImpl;
 
 public class ApiFilter implements Filter {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApiFilter.class);
 	private static final HttpService HTTP_SERVICE = new HttpServiceImpl();
 	private String api_url_token_validation; // https://ser.kong.srm.pw/dashboard/token/validation
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		this.api_url_token_validation = filterConfig.getInitParameter("api_url_token_validation");
-		System.out.println("debug: ******* ApiFilter init: api_url_token_validation=" + api_url_token_validation);
+		LOGGER.info(" ******* ApiFilter init: api_url_token_validation=" + api_url_token_validation);
 	}
 
 	@Override
@@ -36,24 +39,24 @@ public class ApiFilter implements Filter {
 		if (servletRequest instanceof HttpServletRequest) {
 			String requestURL = ((HttpServletRequest) servletRequest).getRequestURL().toString();
 			String queryString = ((HttpServletRequest) servletRequest).getQueryString();
-			System.out.println("debug: doFilter:==>" + requestURL + "?" + queryString);
+			LOGGER.info("doFilter:==>" + requestURL + "?" + queryString);
 		}
 		
-		System.out.println("debug: ******* Before doFilter ******* ");
+		LOGGER.debug(" ******* Before doFilter ******* ");
 		servletResponse.setContentType("application/json");
 		servletResponse.setCharacterEncoding("UTF-8");
 		
 		// Validate the token:
 		String token = servletRequest.getParameter(RootAPI.API_KEY);
 		if (!this.checkToken(token)) {
-			System.out.println("debug: Invalide token=" + token);
+			LOGGER.error("Invalide token=" + token);
 			PrintWriter out = servletResponse.getWriter();
 			String jsonStr = ApiResponse.unauthorizedError().toString();
 			out.print(jsonStr);
 		} else {
 			filterChain.doFilter(servletRequest, servletResponse);
 		}
-		System.out.println("debug: ******* After doFilter ******* ");
+		LOGGER.debug(" ******* After doFilter ******* ");
 	}
 
 	@Override
